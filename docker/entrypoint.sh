76106a983a2bb8f58a5e8667387f0c2f8992db6a
pBorky <pborky@pborky.sk>
2020-11-22 04:51:06 +0100

    Allow credentials to be kept in config volume mounted under /config.

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,16 +1,14 @@
 #!/bin/sh
 
-HTPASSWD_FILE=/etc/nginx/htpasswd
+HTPASSWD_FILE=/config/htpasswd
 
-if [[ -n "${USERNAME}" ]] && [[ -n "${HASHED_PASSWORD}" ]]
-then
-	echo "${USERNAME}:${PASSWORD}" > ${HTPASSWD_FILE}
+if [[ -n "${USERNAME}" ]] && [[ -n "${HASHED_PASSWORD}" ]]; then
+	echo "${USERNAME}:${PASSWORD}" >> ${HTPASSWD_FILE}
 	echo Created htpasswd file with given credentials.
-elif [[ -n "${USERNAME}" ]] && [[ -n "${PASSWORD}" ]]
-then
-	htpasswd -bc ${HTPASSWD_FILE} ${USERNAME} ${PASSWORD}
+elif [[ -n "${USERNAME}" ]] && [[ -n "${PASSWORD}" ]]; then
+	htpasswd -B ${HTPASSWD_FILE} ${USERNAME} ${PASSWORD}
 	echo Hashed given password and created htpasswd file.
-else
+elif [[ ! -e ${HTPASSWD_FILE} ]]; then
 	echo Using no auth.
 	sed -i 's%auth_basic           "Restricted";% %g'        /etc/nginx/conf.d/default.conf
 	sed -i 's%auth_basic_user_file /etc/nginx/htpasswd;% %g' /etc/nginx/conf.d/default.conf
